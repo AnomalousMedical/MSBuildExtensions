552caf2f16982b49aa19188aeba80066d47bbcd7
Andrew Piper <threax@users.noreply.github.com>
2014-03-14 14:55:00 +0000

    Added a build task to setup a mac framework properly.

---
+++ Anomalous/Tasks/RunFailedException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnomalousMSBuild.Tasks
+{
+    class RunFailedException : Exception
+    {
+        public RunFailedException(String message)
+            :base(message)
+        {
+
+        }
+    }
+}
