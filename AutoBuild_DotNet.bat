c3864e6f0453a7938d597a4582abc0658afa2f18
AndrewPiper <AndrewPiper@localhost>
2014-12-26 17:15:32 +0000

    Added multithreaded build to all autobuild.

--- AutoBuild_DotNet.bat
+++ AutoBuild_DotNet.bat
@@ -4,7 +4,7 @@ set SolutionName=Anomalous\AnomalousMSBuild.sln
 ::Less likely to need to change these.
 set ThisFolder=%~dp0
 set RootDependencyFolder=%ThisFolder%..\
-set BuildCommand="C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe"
+set BuildCommand="C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" /m
 set CurrentDirectory=%CD%
 
 set SolutionPath=%ThisFolder%%SolutionName%
