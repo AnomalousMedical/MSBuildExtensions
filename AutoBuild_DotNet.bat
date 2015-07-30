4688fb65820d1c6a1c4bba88b814e60f4f35266d
AndrewPiper <AndrewPiper@localhost>
2015-07-30 20:37:17 +0000

    Fixed target versions from 4.5.2 to 4.5. Fixed dotnet scripts. Updated ogg/vorbis project for vs2015. Added an autobuild for newtonsoft.json, need to hook it into main build scripts.

--- AutoBuild_DotNet.bat
+++ AutoBuild_DotNet.bat
@@ -4,10 +4,9 @@ set SolutionName=Anomalous\AnomalousMSBuild.sln
 ::Less likely to need to change these.
 set ThisFolder=%~dp0
 set RootDependencyFolder=%ThisFolder%..\
-set BuildCommand="C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" /m
 set CurrentDirectory=%CD%
 
 set SolutionPath=%ThisFolder%%SolutionName%
 
-%BuildCommand% "%SolutionPath%" /property:Configuration=Release;Platform="Any CPU" /target:Clean
-%BuildCommand% "%SolutionPath%" /property:Configuration=Release;Platform="Any CPU"
+msbuild.exe "%SolutionPath%" /property:Configuration=Release;Platform="Any CPU" /target:Clean
+msbuild.exe "%SolutionPath%" /property:Configuration=Release;Platform="Any CPU"
